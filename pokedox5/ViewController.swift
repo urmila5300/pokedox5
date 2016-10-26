1f87c6e598b7eb0b273acf01191e1d4793ecd5e8
Jayanth Shetty <jayanthshetty@jayanths-imac.local>
2016-10-26 15:14:49 +0530

    Initial Commit

---
+++ pokedox5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedox5
+//
+//  Created by Jayanth Shetty on 2016-10-26.
+//  Copyright © 2016 Sprouts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
